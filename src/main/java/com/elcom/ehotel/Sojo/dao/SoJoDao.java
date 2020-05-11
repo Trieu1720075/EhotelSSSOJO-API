cc0060988385095c69de4a9ceb3210b1b71dd7a7
Your Name <you@example.com>
2020-05-11 16:53:06 +0700

    Update API

--- src/main/java/com/elcom/ehotel/Sojo/dao/SoJoDao.java
+++ src/main/java/com/elcom/ehotel/Sojo/dao/SoJoDao.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.HttpURLConnection;
 import java.net.InetAddress;
 import java.net.URL;
 import java.util.ArrayList;
